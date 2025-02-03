180cc6e3eac800a9b75fcc4cd7385e45e28ae389
Oliver Jensen <u2422922@rts-365.dk>
2025-02-03 13:47:14 +0100

    Added navbar and footer + styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,26 @@
+import '../style/style.css'
 function Navbar() {
     return ( 
         <>
-        
+         <header>
+        <div class="topnav">
+            <p><i class="fa-solid fa-location-dot"></i></p>
+            <p><i class="fa-solid fa-phone"></i></p>
+            <p><i class="fa-regular fa-envelope"></i></p>
+            <button class="topnav_button"><i class="fa-solid fa-envelope"></i><span class="topnav_black_line">|</span>Subscribe</button>
+        </div>
+        <nav class="navbar">
+            <ul class="navbar_list">
+               <li><a class="navbar_active" href="#">HOME</a></li>
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#">BRANDS</a></li>
+                <li><a href="#">BLOG</a></li>
+                <li><a href="#">EVENTS</a></li>
+                <li><a href="#">SHOP</a></li>
+                <li><a href="#">CONTACT US</a></li>
+            </ul>
+        </nav>
+    </header>
         </>
      );
 }
