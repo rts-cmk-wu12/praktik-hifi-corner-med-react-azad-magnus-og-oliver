278546374197cf7c0537bcfa22f2758cbd006bb7
Oliver Jensen <u2422922@rts-365.dk>
2025-02-04 11:19:45 +0100

    Made Brands site with styling etc

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,48 +1,48 @@
 import '../style/components/header.scss'
-import {LocationMark, Phone, Email} from '../GetIcons.jsx'
-import {NavLink} from 'react-router'
+import { LocationMark, Phone, Email } from '../GetIcons.jsx'
+import { NavLink } from 'react-router'
 
 function Navbar() {
     return (
         <>
             <header>
                 <div className="topnav">
-                    <p><LocationMark/></p>
-                    <p><Phone/></p>
-                    <p><Email/></p>
-                    <button className="topnav__button"><Email/><span className="topnav__black-line">|</span>Subscribe
+                    <p><LocationMark /></p>
+                    <p><Phone /></p>
+                    <p><Email /></p>
+                    <button className="topnav__button"><Email /><span className="topnav__black-line">|</span>Subscribe
                     </button>
                 </div>
                 <nav className="navbar">
                     <ul className="navbar__list">
                         <li>
                             <NavLink to="/"
-                                     className={({isActive}) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>HOME</NavLink>
+                                className={({ isActive }) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>HOME</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about"
-                                     className={({isActive}) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>ABOUT
+                                className={({ isActive }) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>ABOUT
                                 US</NavLink>
                         </li>
                         <li>
                             <NavLink to="/brand-page"
-                                     className={({isActive}) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>BRANDS</NavLink>
+                                className={({ isActive }) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>BRANDS</NavLink>
                         </li>
                         <li>
                             <NavLink to="/blog"
-                                     className={({isActive}) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>BLOG</NavLink>
+                                className={({ isActive }) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>BLOG</NavLink>
                         </li>
                         <li>
                             <NavLink to="/events"
-                                     className={({isActive}) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>EVENTS</NavLink>
+                                className={({ isActive }) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>EVENTS</NavLink>
                         </li>
                         <li>
                             <NavLink to="/product-page"
-                                     className={({isActive}) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>SHOP</NavLink>
+                                className={({ isActive }) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>SHOP</NavLink>
                         </li>
                         <li>
                             <NavLink to="/contact"
-                                     className={({isActive}) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>CONTACT</NavLink>
+                                className={({ isActive }) => `navbar__link navbar__link${isActive ? '--active' : ''}`}>CONTACT</NavLink>
                         </li>
                     </ul>
                 </nav>
