d659fa1bca0fd2b1a82db9714d4c27598d7b84a8
azad0313 <azad123@live.dk>
2025-02-04 09:16:44 +0100

    billeder tilføjet til shop siden

--- src/pages/shop-page.jsx
+++ src/pages/shop-page.jsx
@@ -1,7 +1,97 @@
+
+
+
 const ShopPage = () => {
     return (
-        <><div>
-           
+        <>
+        <div>
+        <div id="grid-container">
+
+<div class="grid-items"> 
+<p class="overlay-text">SHOP BY BRAND</p>
+
+</div>
+
+<div class="grid-items"> 
+<p class="overlay-text">AMPLIFIERS</p>
+
+</div>
+    
+<div class="grid__items"> 
+<p class="overlay-text">SPEAKERS</p>
+
+</div>
+
+<div class="grid__items"> 
+<p class="overlay-text">TURNTABLES</p>
+
+</div>
+
+<div class="grid__items"> 
+<p class="overlay-text">CD PLAYERS</p>
+
+</div>
+
+<div class="grid__items"> 
+<p class="overlay-text">STREAMERS</p>
+
+</div>
+
+
+<div class="grid__items"> 
+<p class="overlay-text">CABLES</p>
+
+</div>
+
+<div class="grid__items"> 
+<p class="overlay-text">FURNITURE</p>
+
+</div>
+
+
+<div class="grid__items"> 
+<p class="overlay-text">HEADPHONES</p>
+
+</div>
+
+
+<div class="grid-items"> 
+<p class="overlay-text">HOME CINEMA</p>
+
+</div>
+
+<div class="grid-items"> 
+<p class="overlay-text">OUTLET - SAVE UP TO 50% OFF</p>
+
+</div>
+
+<div class="grid-items"> 
+<p class="overlay-text">EBAY SHOP</p>
+
+</div>
+
+    
+<div class="grid-items"> 
+<p class="overlay-text">CEILING SPEAKERS</p>
+
+</div>
+
+<div class="grid-items"> 
+<p class="overlay-text">TVS</p>
+</div>
+
+
+<div class="grid-items"> 
+<p class="overlay-text">VINYL LPS</p>
+</div>
+
+
+
+<div id="button" class="grid-items">
+<button type="submit">SHOP NOW</button>   
+</div>
+
+</div>
         </div>
         </>
     )
