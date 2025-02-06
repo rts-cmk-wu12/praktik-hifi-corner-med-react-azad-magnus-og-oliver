083c661f0695190535c90d32f72aac1cf971dbce
azad0313 <azad123@live.dk>
2025-02-06 10:49:55 +0100

    shop-page added new pictures and about page is adedd

--- src/pages/shop-page.jsx
+++ src/pages/shop-page.jsx
@@ -11,64 +11,63 @@ const ShopPage = () => {
 
 <div class="grid__items"> 
 <p class="overlay-text">SHOP BY BRAND</p>
-<img src="/img/SHOP BY BRAND.jpg"></img>
+<img src="/img/shop-13.jpg"></img>
 </div>
 
 <div class="grid__items"> 
 <p class="overlay-text">AMPLIFIERS</p>
-<img src="/img/AMPLIFIERS.jpg"></img>
+<img src="/img/shop-6.jpg"></img>
 </div>
     
 <div class="grid__items"> 
 <p class="overlay-text">SPEAKERS</p>
-<img src="/img/SPEAKER.jpg"></img>
+<img src="/img/shop-2.jpg"></img>
 
 </div>
 
 <div class="grid__items"> 
 <p class="overlay-text">TURNTABLES</p>
-<img src="/img/TURNTABLES.jpg"></img>
+<img src="/img/shop-5.jpg"></img>
 
 </div>
 
 <div class="grid__items"> 
 <p class="overlay-text">CD PLAYERS</p>
-<img src="/img/cdplayer2.jpg"></img>
+<img src="/img/shop-8.jpg"></img>
 </div>
 
 <div class="grid__items"> 
 <p class="overlay-text">STREAMERS</p>
-<img src="/img/COLOURBOX14811521.jpg"></img>
+<img src="/img/shop-7.jpg"></img>
 </div>
 
 
 <div class="grid__items"> 
 <p class="overlay-text">CABLES</p>
-<img src="/img/CABLES.jpg"></img>
+<img src="/img/shop-9.jpg"></img>
 
 </div>
 
 <div class="grid__items"> 
 <p class="overlay-text">FURNITURE</p>
-<img src="/img/FURNITURE.jpg"></img>
+<img src="/img/shop-13.jpg"></img>
 </div>
 
 
 <div class="grid__items"> 
 <p class="overlay-text">HEADPHONES</p>
-<img src="/img/headphones3.jpg"></img>
+<img src="/img/shop-14.jpg"></img>
 </div>
 
 
 <div class="grid__items"> 
 <p class="overlay-text">HOME CINEMA</p>
-<img className="color-none" src="/img/HOME CINEMA.jpg"></img>
+<img src="/img/shop-11.jpg"></img>
 </div>
 
 <div class="grid__items"> 
 <p class="overlay-text">OUTLET - SAVE UP TO 50% OFF</p>
-<img className="color-red-on-pic" src="/img/outlet50.jpg"></img>
-</div>
+<img src="/img/shop-15.jpg"></img></div>
 
 <div class="grid__items"> 
 <p class="overlay-text">EBAY SHOP</p>
@@ -78,18 +77,17 @@ const ShopPage = () => {
     
 <div class="grid__items"> 
 <p class="overlay-text">CEILING SPEAKERS</p>
-<img src="/img/CEILING SPEAKER.jpg"></img>
-</div>
+<img src="/img/shop-2.jpg"></img></div>
 
 <div class="grid__items"> 
 <p class="overlay-text">TVS</p>
-<img className="color-none" src="/img/TV.jpg"></img>
+<img src="/img/shop-11.jpg"></img>
 </div>
 
 
 <div class="grid__items"> 
 <p class="overlay-text">VINYL LPS</p>
-<img className="color-none" src="/img/vinyl3.jpg"></img>
+<img src="/img/shop-5.jpg"></img>
 </div>
 
 
