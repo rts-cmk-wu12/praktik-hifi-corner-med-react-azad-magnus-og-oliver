385dfe90cec99d40a9deaa32797dd4d99e8bae57
azad0313 <azad123@live.dk>
2025-02-07 10:40:00 +0100

    returns-refund page created and link added to the footer and privacy page created

---
+++ src/pages/returns-refunds.jsx
@@ -0,0 +1,84 @@
+import "../style/returns-refunds.scss";
+import Footer from "../components/footer"
+import Navbar from "../components/navbar"
+
+
+const ReturnPage = () => {
+    return (
+        <>
+        <Navbar />
+       <div className="returns-refunds-box">
+       <div className="pic-in-return">
+        <h1 className="headline-in-return">Returns & Refunds</h1>
+        </div>
+
+<section className="section-with-healines-and-text">
+<h2>delivery</h2>
+<p>Please check the condition of all products delivered as soon as they are received. This does not affect your statutory rights. If there is a problem we would like to deal with this as soon as possible to alleviate any inconvenience to yourself.
+
+As soon as we have delivered the goods to you, you will be responsible for them and any damage occasioned whilst in your possession. The majority of goods we send will require you to sign for them upon delivery. If you are not able to sign for a delivery, we will do our utmost to contact you to arrange when it is convenient to redeliver. Our responsibility for everything other than damage due to our negligence will end at the time the carrier tried to delivery the goods to you. If your product is visibly damaged in transit, please sign for the delivery as ‘damaged’ and let us know as soon as possible.</p>
+</section>
+
+
+<section className="section-with-healines-and-text">
+<h2>our guarantee</h2>
+<p>All items excluding ex-display and secondhand goods are covered by at least 12 months warranty; this is detailed on your receipt. Any secondhand or ex-display product is covered by at least 3 months warranty as noted in the original listing.
+
+Hi-Fi Corner cannot guarantee against any misuse (i.e. blown speakers etc.), commercial use, wear and tear, erosion of parts, loss of data stored on any form of writeable/hard drive devices, dead pixels of an amount not covered by the manufacturer’s specifications or where you have not allowed repairs of goods to be carried out by authorised agents. If it has been more than 30 days since you have received a product and discovered a fault, we will still repair the goods free of charge or, if you prefer, replace them provided that you report the fault within 12 months of purchase. If we are unable to repair or replace the item(s) we will provide you with a full refund instead.
+
+Please note that if it is after the initial 30 days of purchase you must be able to certify the fault was present upon delivery and not by misuse.</p>
+</section>
+
+<section className="section-with-healines-and-text">
+<h2>returns process/your statutory right</h2>
+<p>If you wish to return goods purchased online or over the phone, you should notify Hi-Fi Corner of your intention to do so within 14 days of the date of receipt.
+
+You then have an additional 14 days to return the goods from this notification date (goods must be returned within 28 days). We will in such circumstances refund you the cost of your order, excluding postage or delivery cost. However, if goods are not returned “as new” due to excessive handling on your part, or with components missing (including packing materials and manuals) we may make a reasonable deduction for this. Please do not send your product back to the manufacturer. We kindly ask that to make the refund process easier, that you include your receipt or proof of purchase and a cover letter detailing briefly why the item is being returned. We cannot refund the cost of delivery fees if there was an additional charge, only the cost of the goods themselves. We will process the refund when the item arrives with us. If you are shipping an item over £100, we would advise using a trackable shipping service or purchasing shipping insurance. We cannot guarantee that we will receive your returned item.
+
+Please contact us before attempting to return your goods. For purchases bought in-store, you can also drop in and be refunded instantly.
+
+</p>
+</section>
+
+<section className="section-with-healines-and-text">
+<h2>refunds</h2>
+<p>Once your returned item is received and inspected, we will send you an email or phone call to notify you that we have received your returned item.We will also notify you of the approval or rejection of your refund.
+
+If you are approved, then your refund will be processed, and a credit will automatically be applied to your original method of payment, within 3 working days. Please note we require an email address or contact number to arrange a refund that was purchased over the phone. There are certain situations where only partial refunds are granted or we cannot refund you; Any item not in its original condition, is damaged or missing parts for reasons not due to our error. Any damage that has been caused by misuse, such as a speaker drive unit being blown or water being introduced to the unit, or that has been damaged due to poor packing. Any item that is returned more than 30 days after delivery. In any instance, we will notify you as soon as possible to resolve this matter.
+
+If you haven’t received a refund after confirmation, first check your bank account again. Then contact your credit card company, it may take some time before your refund is officially posted. Next contact your bank. There is often some processing time of a couple of days before a refund is posted. If you have done all of this and you still have not received your refund after 7 days, please contact us at sales@hificorner.co.uk.</p>
+</section>
+
+<section className="section-with-healines-and-text">
+<h2>sale items</h2>
+<p>Only regularly priced items may be refunded, unfortunately clearance, special order and outlet items cannot be refunded unless they are found to be faulty on arrival. For more details about returning secondhand or sale items, please contact sales@hificorner.co.uk.</p>
+</section>
+
+<section className="section-with-healines-and-text">
+<h2>exchanges</h2>
+<p>We can only replace items if they are defective or damaged. If you need to exchange the product for the same item due to a fault, please first send us an email at sales@hificorner.co.uk and once approved, send your item to:
+
+Hi-Fi Corner, 2 Joppa Road, Mackenzie House, Portobello, Edinburgh, EH15 2EU.
+
+Please also include the proof of purchase and a cover letter detailing the reason for exchange.</p>
+</section>
+
+<section className="section-with-healines-and-text">
+<h2>shipping</h2>
+<p>To return your product, you should mail your product to:
+2 Joppa Road, Mackenzie House, Edinburgh, EH15 2EU.
+If you would like to return an item, please note that we cannot arrange a collection of the item without additional cost; you will be responsible for ensuring the product reaches us. Shipping costs are non-refundable. If you receive a refund, the cost of return shipping will be deducted from your refund. Depending on where you live, the time it may take for your exchanged product to reach you may vary. We will process the refund when the item arrives with us.
+
+If you are shipping an item over £100, we would advise using a trackable shipping service or purchasing shipping insurance. We cannot guarantee that we will receive your returned item.</p>
+</section>
+
+
+
+
+       </div>
+        <Footer/>
+        </>
+    )
+}
+
+export default ReturnPage
