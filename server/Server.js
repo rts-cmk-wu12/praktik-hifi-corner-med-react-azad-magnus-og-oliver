2f3dfd2777dc83e45be5fee7e164211d7a840bfc
Sh3dow-ware <u2486896@rts-365.dk>
2025-02-19 10:11:15 +0100

    Fixed ES modules

--- server/Server.js
+++ server/Server.js
@@ -21,6 +21,11 @@ app.use(express.json());
 app.use(cors({
     origin: true,
 }));
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 
