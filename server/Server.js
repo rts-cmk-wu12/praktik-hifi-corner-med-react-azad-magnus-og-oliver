90ca95ffebe72f925ddb7126bf2468253c982426
Sh3dow-ware <u2486896@rts-365.dk>
2025-02-19 13:26:26 +0100

    Fixed

--- server/Server.js
+++ server/Server.js
@@ -28,12 +28,6 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
 app.use(limiter);
 
 const API_KEY = process.env.MONGO_DB_KEY;
--- src/pages/shop-category-page.jsx
+++ src/pages/shop-category-page.jsx
@@ -1,10 +1,10 @@
 import SearchNav from "../components/searchnav"
+import frontpageimge from "/design/hifi-shop-category-list.png"
 const ShopCategoryPage = () => {
     return (
         <>
         <SearchNav/>
         <div>
-            Shop Category Page
         </div>
         </>
     )
