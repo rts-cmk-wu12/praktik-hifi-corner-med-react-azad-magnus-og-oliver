e375ed55f2bf3f4c61d00aa227898cb04c93053f
azad0313 <azad123@live.dk>
2025-02-20 14:00:21 +0100

    added filters section and footer

--- src/pages/shop-category-page.jsx
+++ src/pages/shop-category-page.jsx
@@ -4,17 +4,17 @@ import Footer from "../components/footer";
 import { ShopProductCard } from "../components/ShopProductCard";
 
 const ShopCategoryPage = () => {
-    const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-    return (
-        <>
-            <SearchNav onSearch={setSearchTerm} />
-            <div>
-                <ShopProductCard onSearch={searchTerm} />
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <SearchNav onSearch={setSearchTerm} />
+      <div>
+        <ShopProductCard onSearch={searchTerm} />
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default ShopCategoryPage;
