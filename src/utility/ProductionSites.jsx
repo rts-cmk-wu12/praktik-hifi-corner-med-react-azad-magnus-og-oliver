10b1ed972f30cb1c2a7ab97c5726fd5a958e1bed
Sh3dow-ware <u2486896@rts-365.dk>
2025-02-20 11:09:48 +0100

    Fixed API Url.

--- src/utility/ProductionSites.jsx
+++ src/utility/ProductionSites.jsx
@@ -1,4 +1,1 @@
-const isProduction = window.location.hostname.includes('localhost');
-export const apiUrl = isProduction
-    ? 'http://localhost:3000/'  // Production URL
-    : 'https://praktik-hifi-corner-med-react-azad-magnus-og-oliver.vercel.app/';
+export const apiUrl = "https://render-db-server.onrender.com/"
